116b136383ca44813f1557e4c7046b68b6c0e978
Mahdi Hazrati <127740921+Mahdi-Hazrati@users.noreply.github.com>
2025-01-14 22:16:49 +0330

    add umami

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className} overflow-y-scroll overflow-x-hidden`}>{children}</body>
+      <script defer src="https://cloud.umami.is/script.js" data-website-id="d733939c-b8f0-4b79-aeb6-19b27a21dbd4"></script>
     </html>
   );
 }
