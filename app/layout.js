e278801020ae63208c512b8cb2a66c2f5c91e240
Mahdi Hazrati <127740921+Mahdi-Hazrati@users.noreply.github.com>
2025-01-22 23:34:33 +0330

    add platform track code

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Next Production",
+  title: "Next Production Dev",
   description: "Next Production try to make next big thing",
 };
 
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className} overflow-y-scroll overflow-x-hidden`}>{children}</body>
-      <script defer src="https://cloud.umami.is/script.js" data-website-id="d733939c-b8f0-4b79-aeb6-19b27a21dbd4"></script>
+      <script defer src="https://platform.analytick.ir/script.js" data-website-id="4dd27363-d5ac-454b-b1b2-dacde74db9ac"></script>
     </html>
   );
 }
